385658ec4e9ede9fba83b4819367e5acd4659e20
angusbayley <angusbayley@gmail.com>
2021-04-10 14:08:15 +0100

    remove stuff I won't need, get file structure set up

---
+++ src/components/StatusBar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+const StatusBar = () => {
+  return (
+    <div className="StatusBar-container">
+      hello
+    </div>
+  )
+}
+
+export default StatusBar;
